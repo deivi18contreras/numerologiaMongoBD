90837e4a69ff8ecd2ddbae11c74e871db6fb139e
deivi18contreras <deivibogoya@gmail.com>
2026-03-19 16:50:30 -0500

    Configuración final de mercado pago con credenciales de produccion

--- front/src/plugins/pluginAxios.js
+++ front/src/plugins/pluginAxios.js
@@ -3,7 +3,7 @@ import { useAuthStore } from "../store/Auth.js";
 
 const axiosInstance = axios.create({
 
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL ,
   headers: {
     "Content-Type": "application/json"
   }
